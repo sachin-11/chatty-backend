9c729fd23ac903a9a6b2c311f806edec308a70a0
sachin <sachin.singh@manektech.com>
2022-12-16 20:07:39 +0530

    work on auth module

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,5 @@
+import { authRoutes } from '@auth/routes/authRoutes';
 import { Application } from 'express';
-// import { authRoutes } from '@auth/routes/authRoutes';
-// import { serverAdapter } from '@service/queues/base.queue';
-// import { currentUserRoutes } from '@auth/routes/currentRoutes';
-// import { authMiddleware } from '@global/helpers/auth-middleware';
-// import { postRoutes } from '@post/routes/postRoutes';
-// import { reactionRoutes } from '@reaction/routes/reactionRoutes';
-// import { commentRoutes } from '@comment/routes/commentRoutes';
-// import { followerRoutes } from '@follower/routes/followerRoutes';
-// import { notificationRoutes } from '@notification/routes/notificationRoutes';
-// import { imageRoutes } from '@image/routes/imageRoutes';
-// import { chatRoutes } from '@chat/routes/chatRoutes';
-// import { userRoutes } from '@user/routes/userRoutes';
-// import { healthRoutes } from '@user/routes/healthRoutes';
 
 const BASE_PATH = '/api/v1';
 
@@ -22,7 +10,7 @@ export default (app: Application) => {
     // app.use('', healthRoutes.env());
     // app.use('', healthRoutes.instance());
     // app.use('', healthRoutes.fiboRoutes());
-    // app.use(BASE_PATH, authRoutes.routes());
+    app.use(BASE_PATH, authRoutes.routes());
     // app.use(BASE_PATH, authRoutes.signoutRoute());
     // app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
     // app.use(BASE_PATH, authMiddleware.verifyUser, postRoutes.routes());
