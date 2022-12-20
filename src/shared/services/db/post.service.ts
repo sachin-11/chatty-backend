0f0858e67234ade6fc88b53b1bf90f2e0018778e
sachin <sachin.singh@manektech.com>
2022-12-20 17:41:49 +0530

    post feature implemented

--- src/shared/services/db/post.service.ts
+++ src/shared/services/db/post.service.ts
@@ -11,35 +11,35 @@ class PostService {
     await Promise.all([post, user]);
   }
 
-  // public async getPosts(query: IGetPostsQuery, skip = 0, limit = 0, sort: Record<string, 1 | -1>): Promise<IPostDocument[]> {
-  //   let postQuery = {};
-  //   if (query?.imgId && query?.gifUrl) {
-  //     postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] };
-  //   } else if (query?.videoId) {
-  //     postQuery = { $or: [{ videoId: { $ne: '' } }] };
-  //   } else {
-  //     postQuery = query;
-  //   }
-  //   const posts: IPostDocument[] = await PostModel.aggregate([{ $match: postQuery }, { $sort: sort }, { $skip: skip }, { $limit: limit }]);
-  //   return posts;
-  // }
+  public async getPosts(query: IGetPostsQuery, skip = 0, limit = 0, sort: Record<string, 1 | -1>): Promise<IPostDocument[]> {
+    let postQuery = {};
+    if (query?.imgId && query?.gifUrl) {
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] };
+    } else if (query?.videoId) {
+      postQuery = { $or: [{ videoId: { $ne: '' } }] };
+    } else {
+      postQuery = query;
+    }
+    const posts: IPostDocument[] = await PostModel.aggregate([{ $match: postQuery }, { $sort: sort }, { $skip: skip }, { $limit: limit }]);
+    return posts;
+  }
 
-  // public async postsCount(): Promise<number> {
-  //   const count: number = await PostModel.find({}).countDocuments();
-  //   return count;
-  // }
+  public async postsCount(): Promise<number> {
+    const count: number = await PostModel.find({}).countDocuments();
+    return count;
+  }
 
-  // public async deletePost(postId: string, userId: string): Promise<void> {
-  //   const deletePost: Query<IQueryComplete & IQueryDeleted, IPostDocument> = PostModel.deleteOne({ _id: postId });
-  //   // delete reactions here
-  //   const decrementPostCount: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: -1 } });
-  //   await Promise.all([deletePost, decrementPostCount]);
-  // }
+  public async deletePost(postId: string, userId: string): Promise<void> {
+    const deletePost: Query<IQueryComplete & IQueryDeleted, IPostDocument> = PostModel.deleteOne({ _id: postId });
+    // delete reactions here
+    const decrementPostCount: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: -1 } });
+    await Promise.all([deletePost, decrementPostCount]);
+  }
 
-  // public async editPost(postId: string, updatedPost: IPostDocument): Promise<void> {
-  //   const updatePost: UpdateQuery<IPostDocument> = PostModel.updateOne({ _id: postId }, { $set: updatedPost });
-  //   await Promise.all([updatePost]);
-  // }
+  public async editPost(postId: string, updatedPost: IPostDocument): Promise<void> {
+    const updatePost: UpdateQuery<IPostDocument> = PostModel.updateOne({ _id: postId }, { $set: updatedPost });
+    await Promise.all([updatePost]);
+  }
 }
 
 export const postService: PostService = new PostService();
--- src/shared/workers/post.worker.ts
+++ src/shared/workers/post.worker.ts
@@ -18,29 +18,29 @@ class PostWorker {
     }
   }
 
-  // async deletePostFromDB(job: Job, done: DoneCallback): Promise<void> {
-  //   try {
-  //     const { keyOne, keyTwo } = job.data;
-  //     await postService.deletePost(keyOne, keyTwo);
-  //     job.progress(100);
-  //     done(null, job.data);
-  //   } catch (error) {
-  //     log.error(error);
-  //     done(error as Error);
-  //   }
-  // }
+  async deletePostFromDB(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const { keyOne, keyTwo } = job.data;
+      await postService.deletePost(keyOne, keyTwo);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
 
-  // async updatePostInDB(job: Job, done: DoneCallback): Promise<void> {
-  //   try {
-  //     const { key, value } = job.data;
-  //     await postService.editPost(key, value);
-  //     job.progress(100);
-  //     done(null, job.data);
-  //   } catch (error) {
-  //     log.error(error);
-  //     done(error as Error);
-  //   }
-  // }
+  async updatePostInDB(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const { key, value } = job.data;
+      await postService.editPost(key, value);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
 }
 
 export const postWorker: PostWorker = new PostWorker();
